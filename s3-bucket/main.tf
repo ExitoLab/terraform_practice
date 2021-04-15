ce7e8137cb02b56b6cd0f909870b025537f9662b
Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>
2021-04-15 22:44:56 +0400

    Add support for versioning s3 bucket
    Signed-off-by: Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>

--- s3-bucket/main.tf
+++ s3-bucket/main.tf
@@ -1,7 +1,10 @@
 resource "aws_s3_bucket" "terraform_pratice" {
   bucket        = "terraform-practice"
   acl           = "private"
-  force_destroy = true
+
+  versioning {
+    enabled = true
+  }
 
   tags = {
     Name = "S3 Remote Terraform State Store"
