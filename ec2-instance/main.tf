b7c553eca5f6dc39b598754bb68b0c1d32bd6ba8
Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>
2021-05-01 23:30:08 +0400

    fix: use terraform 14 syntax
    Signed-off-by: Ige Adetokunbo Temitayo <igeadetokunbo@gmail.com>

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -1,7 +1,7 @@
 #create key pair on aws
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ssh_key"
-  public_key = "${var.public_key}"
+  public_key = var.public_key
   tags       = local.tags
 }
 
